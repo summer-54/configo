88f0b063ae87f850b4de46e3529416e9c6be6e4b
Yankovskiy Dmitriy <dmityank@gmail.com>
2025-11-06 18:41:28 +0300

    v1

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use serde::{Deserialize, Serialize};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait Config: Default + Serialize + for<'de> Deserialize<'de> {
+    const NAME: &'static str;
+    fn load(dir: &str) -> impl Future<Output = Self> {
+        async move {
+            let path = format!("{dir}/{}.yaml", Self::NAME).into_boxed_str();
+            if !tokio::fs::try_exists(&*path).await.unwrap() {
+                let this = Self::default();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+                tokio::fs::write(&*path, serde_yml::to_string(&this).unwrap())
+                    .await
+                    .unwrap();
+
+                log::warn!("'{}' config not found by path: {path}", Self::NAME);
+                log::info!(
+                    "{} config was automaticly created by path: {path}",
+                    Self::NAME
+                );
+
+                this
+            } else {
+                let this =
+                    serde_yml::from_str(&tokio::fs::read_to_string(&*path).await.unwrap()).unwrap();
+                log::trace!("{} config was loaded", Self::NAME);
+                this
+            }
+        }
     }
 }
