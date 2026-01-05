54d60510e2155bf04684118c5a32516dc2282f9f
Yankovskiy Dmitriy <dmityank@gmail.com>
2026-01-05 13:18:35 +0300

    bug fixed

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use colored::Colorize as _;
 use serde::{Deserialize, Serialize};
+use tokio::io::{AsyncWriteExt, BufWriter};
 
 pub trait Config: Default + Serialize + for<'de> Deserialize<'de> {
     const NAME: &'static str;
@@ -8,11 +9,10 @@ pub trait Config: Default + Serialize + for<'de> Deserialize<'de> {
             let path = format!("{dir}/{}.yaml", Self::NAME).into_boxed_str();
             if !tokio::fs::try_exists(&*path).await.unwrap() {
                 let this = Self::default();
-
-                tokio::fs::write(&*path, serde_yml::to_string(&this).unwrap())
+                let mut file = tokio::fs::File::create(&*path).await.unwrap();
+                file.write_all(serde_yml::to_string(&this).unwrap().as_bytes())
                     .await
                     .unwrap();
-
                 log::warn!(
                     "'{}' config not found by path: {path}",
                     Self::NAME.bold().cyan()
